9a3275622222c61da3081d5e52bf356261be7aa9
VitorC0sta <vitor.costafelippe@gmail.com>
2023-07-11 15:59:17 -0300

    Formating page

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,29 @@ import { DivData } from './components/Div.chardata'
 import { DivImg } from './components/Div.img'  
 import { useEffect, useState } from 'react'
 import { api } from './services/api'
+import './styles/font.css'
 
 function App() {
-  const [characters, setCharacters] = useState(null); // Lista dos personagens conmsumidos pela API
+  const [characters, setCharacters] = useState(null); // Lista dos personagens conmsumidos pela API.
   const [characterIndex, setCharacterIndex] = useState(1); //Índice selecionado.
-  const [character, setCharacter] = useState({}); // Informações do personagem em relação ao indice selecionado.
+  const [character, setCharacter] = useState({}); // Informações do personagem em relação ao índice selecionado.
 
-  useEffect(() => { // resgatando 6 personagens da API
-    api.get('/character/1,2,3,4,5,6').then((response) => {
+  useEffect(() => { // Resgatando 6 personagens da API.
+    api.get('/character/1,2,3,16,13,21').then((response) => {
       if(response.data != null) setCharacters(response.data); //validação se há algum dado de um personagem.
       const tempCharacter = response.data.find(item => item.id === characterIndex);
       if(characterIndex === 1) setCharacter({...tempCharacter, bgcolor: '#85F217'});
-      else if(characterIndex === 2) setCharacter({...tempCharacter, bgcolor: '#FF00EE'});
-      else if(characterIndex === 3) setCharacter({...tempCharacter, bgcolor: '#DFFF00'});
+      else if(characterIndex === 3) setCharacter({...tempCharacter, bgcolor: '#FF00EE'});
+      else if(characterIndex === 2) setCharacter({...tempCharacter, bgcolor: '#DFFF00'});
       else setCharacter({...tempCharacter, bgcolor: '#17EAF2'});
     })
-  }, []); //ao carregar a página.
+  }, []); //Ao carregar a página.
 
- useEffect(() => { //Setando o nome do personagem no titulo principal das informações.
+ useEffect(() => { //Setando o nome do personagem no título principal das informações e cor dos.
     if(characters != null) {
       if(characterIndex === 1) setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#85F217'});
-      else if(characterIndex === 2) setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#FF00EE'});
-      else if(characterIndex === 3) setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#DFFF00'});
+      else if(characterIndex === 3) setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#FF00EE'});
+      else if(characterIndex === 2) setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#DFFF00'});
       else setCharacter({...characters.find(item => item.id === characterIndex), bgcolor: '#17EAF2'});
     }
   }, [characterIndex]); //quando alterar o índice do personagem.
@@ -41,7 +42,7 @@ function App() {
             <>               
               {characters.map((item, key) => {
                   return (                  //Mudando o background de acordo com o personagem.
-                    <CharacterOption style={item.id == characterIndex ? {backgroundColor: character.bgcolor, color:"#121212"} : {backgroundColor: "#3A3A3A"} } onClick={() => setCharacterIndex(item.id)} key={key}> {item.name} </CharacterOption>
+                    <CharacterOption style={item.id == characterIndex ? {backgroundColor: character.bgcolor, color:"#121212", fontWeight:"bold"} : {backgroundColor: "#3A3A3A"} } onClick={() => setCharacterIndex(item.id)} key={key}> {item.name} </CharacterOption>
                   )
                 })
               }
@@ -50,16 +51,35 @@ function App() {
         </ContainerMenu>
           <ContainerInfo> 
             <BoxInfo>
-              <DivData>  
-                
-                <h2 style={{color: character.bgcolor}}>Dados do personagem</h2>
+              <DivData>
+                {console.log(character)}
+                <h1>{character.name}</h1>
+                <table>
+                  <caption style={{color: character.bgcolor}}>Dados do personagem</caption>
+                  <thead>
+                    <tr>
+                      <th style={{borderRadius:"4px 0px 0px 4px"}}> Status </th>
+                      <th> Species </th>
+                      <th> Gender </th>
+                      <th style={{borderRadius:"0px 4px 4px 0px"}}> Origin </th>
+                    </tr>
+                  </thead>
+                  <tbody >
+                    <tr style={{marginTop: "2px"}}>
+                      <td style={{borderRadius:"4px 0px 0px 4px"}}> {character.status} </td>
+                      <td> {character.species} </td>
+                      <td> {character.gender} </td>
+                      <td style={{borderRadius:"0px 4px 4px 0px"}}> </td>
+                    </tr>
+                  </tbody>
+                </table>
               </DivData>
-              <DivImg>
-                Imagem
+              <DivImg style={{display: "flex",justifyContent:"center", alignItems: "center"}}>
+                <img src={character.image} alt="api.img" style={{borderColor: character.bgcolor, boxShadow: character.sdwcolor }}/>
               </DivImg>
             </BoxInfo>
             <BoxInfo>
-              <h2 style={{color: character.bgcolor}}>Aparições por mês</h2>
+              <h2 style={{color: character.bgcolor, marginLeft:"5%"}}>Aparições por mês</h2>
               {/* graph */}
             </BoxInfo>
           </ContainerInfo>
--- src/components/WrapBox.jsx
+++ src/components/WrapBox.jsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
 
 export const WrapBox = styled.div`
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 
-    font-family: 'Montserrat', sans-serif;
     color: #FFFFFF;
     display: flex;
     box-sizing: content-box;
